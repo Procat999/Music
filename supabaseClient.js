1eec27329227280f7cb05548bd41557e23fb0dcf
Procat999 <Procat9999@gmail.com>
2026-03-06 01:07:04 -0400

    Delete supabaseClient.js

--- supabaseClient.js
+++
@@ -1,7 +0,0 @@
-// supabaseClient.js
-import { createClient } from "@supabase/supabase-js"
-
-const supabaseUrl = "https://zqgrdbmqlszjsrepnvcx.supabase.co"
-const supabaseKey = "sb_publishable_DjE9ANlxRd1AmDshacLfrw_VjV-_y7f"
-
-export const supabase = createClient(supabaseUrl, supabaseKey)
