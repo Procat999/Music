8b5689f788f19b11e7a86699d761839f6dbb8fc8
Procat999 <Procat9999@gmail.com>
2026-03-06 02:36:59 -0400

    Delete js directory

--- js/supabase.js
+++
@@ -1,7 +0,0 @@
-// js/supabase.js
-import { createClient } from "@supabase/supabase-js";
-
-const SUPABASE_URL = "https://zqgrdbmqlszjsrepnvcx.supabase.co";
-const SUPABASE_KEY = "sb_publishable_DjE9ANlxRd1AmDshacLfrw_VjV-_y7f";
-
-export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
