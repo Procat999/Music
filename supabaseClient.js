d359a2cf03952c9ce8a5ea8976a69c64299eca4a
Procat999 <Procat9999@gmail.com>
2026-03-06 00:25:44 -0400

    Update supabaseClient.js

--- supabaseClient.js
+++ supabaseClient.js
@@ -1,7 +1,7 @@
 import { createClient } from "@supabase/supabase-js"
 
-const supabaseUrl = "YOUR_SUPABASE_URL"
-const supabaseKey = "YOUR_PUBLIC_KEY"
+const supabaseUrl = "https://zqgrdbmqlszjsrepnvcx.supabase.co"
+const supabaseKey = "sb_publishable_DjE9ANlxRd1AmDshacLfrw_VjV-_y7f"
 
 export const supabase = createClient(
  supabaseUrl,
